05348ecf8cc2982ff4f8db828183b8779037eb15
Moth <danielmotillamonreal@outlook.com>
2026-01-04 16:41:02 -0600

    fix config

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        blog: './blog.html'
+      }
+    }
+  },
+  server: {
+    port: 3000
+  }
+})
